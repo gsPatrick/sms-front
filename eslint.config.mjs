d37775df102345728288c55cc015fb5988bd7536
unknown <patrickgsiqueira@hotmail.com>
2025-07-11 20:30:24 -0300

    js-2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // Desativa erro de variável não usada
+      "import/no-anonymous-default-export": "warn", // Opcional: transforma erro em aviso
+    },
+  },
 ];
 
 export default eslintConfig;
