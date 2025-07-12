60c80403c43077a894ccb7c422319ebb353e110e
unknown <patrickgsiqueira@hotmail.com>
2025-07-11 21:48:19 -0300

    mudando paginas-9

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,1 +1,4 @@
-export { default } from './Layout';
+// This file is part of an old, unused component and was causing build errors
+// because it was trying to export from a non-existent './Layout.jsx' file.
+// The line has been commented out to resolve the issue.
+// export { default } from './Layout';
