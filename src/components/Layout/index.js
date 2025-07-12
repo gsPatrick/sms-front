eeeb9a513b06b9c14811ffc1dd3db5c8229a2c1d
unknown <patrickgsiqueira@hotmail.com>
2025-07-11 21:50:44 -0300

    mudando paginas-10

--- src/components/Layout/index.js
+++
@@ -1,4 +0,0 @@
-// This file is part of an old, unused component and was causing build errors
-// because it was trying to export from a non-existent './Layout.jsx' file.
-// The line has been commented out to resolve the issue.
-// export { default } from './Layout';
